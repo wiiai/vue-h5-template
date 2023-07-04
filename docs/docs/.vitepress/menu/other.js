3ac99128b74d0b977b322ccf782b6009b303b6bc
GaoXiong <xiong.gao@shopee.com>
2023-07-04 15:03:32 +0800

    chore: update

--- docs/docs/.vitepress/menu/other.js
+++
@@ -1,6 +0,0 @@
-export default {
-  text: '其他',
-  collapsible: true,
-  collapsed: true,
-  items: [{ text: '常见问题', link: '/guide/other/' }],
-};
--- docs/docs/.vitepress/menu/preface.js
+++
@@ -1,9 +0,0 @@
-export default {
-  text: '前言',
-  collapsible: true,
-  items: [
-    { text: '介绍', link: '/guide/' },
-    { text: '快速上手', link: '/guide/start' },
-    { text: '参与编辑', link: '/guide/edit' },
-  ],
-};
